0d384dcffec35b2123b42657f2bf7d5c47a0c4c5
SuatBatur <msuatbatur@gmail.com>
2021-10-20 14:17:12 +0300

    student model created

--- studentregister/models.py
+++ studentregister/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models.aggregates import Max
 
 # Create your models here.
+
+class Student(models.Model):
+   name = models.CharField( max_length=100)
+   mobile =models.CharField(max_length=20) 
+   email =models.CharField(max_length=100)
+   GENDER = (
+        ("Female" , "Female"),
+        ("Male" , "Male"),
+        ("Other" , "Other"),
+        ("Prefer Not to" , "Prefer Not to"),
+    )
+   gender = models.CharField(max_length=50, choices=GENDER)
+   
+   number = models.CharField(max_length=50)
+   PATH =(
+       ("Front-End", "Front-End"),
+       ("Back-End", "Back-End"),
+       ("Data Analys", "Data Analys"),
+       ("Data Science", "Data Science"),
+       ("AWS-DevOps", "AWS-DevOps")
+   )
+   
+   path = models.CharField(max_length=50, choices=PATH)
+   
